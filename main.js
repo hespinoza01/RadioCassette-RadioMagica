9a527397c7987851075427a971ff8bf8c34b8edd
Harold Espinoza <haroldesptru@gmail.com>
2020-05-28 17:50:03 -0600

    Completada funcionabilidad básica del reproductor. Pendiente el movimiento de la cinta según avanza el tiempo de reproducción

--- main.js
+++ main.js
@@ -187,11 +187,468 @@ const RadioCassette = {
     valorTiempoGeneral: "01:01:00", // Tiempo de duración en segundos para el tipo de reproducción general, 1 hora = 3600 segundos
     estiloReproduccion: 1, // Tipo de reproducción 1=inicio a fin, 2=inicio a fin y repetir, 3=revolver lista, 4=sattolo
     tiempoFinal: 1, // Tipo de tiempo final 1=timepo total, 2=tiempo restante
-    imagenTitulo: '', // url de la imagen a mostrar detrás del texto del título de la emisora
-    textoTitulo: '', // Texto a mostrar como título de la emisora
-    imagenEslogan: '', // url de la imagen a mostrar detrás del texto del eslogan de la emisora
-    textoEslogan: '', // Texto a mostrar como eslogan de la emisora
+    imagenTitulo: 'https://i.pinimg.com/originals/83/81/17/8381171693f7fedc6b6411c39f15f9fb.jpg', // url de la imagen a mostrar detrás del texto del título de la emisora
+    textoTitulo: 'Radio Maracaibo', // Texto a mostrar como título de la emisora
+    imagenEslogan: 'https://i.pinimg.com/originals/83/81/17/8381171693f7fedc6b6411c39f15f9fb.jpg', // url de la imagen a mostrar detrás del texto del eslogan de la emisora
+    textoEslogan: 'la radio alegre', // Texto a mostrar como eslogan de la emisora
 };
 
+/*
+*    Eventos
+*/
+// Convertir los segundos a los valores de tiempo legibles
+function getReadableTime(duration) {
+    duration = parseInt(duration);
+
+    let horas = Math.floor(duration / 3600),
+        minutos = Math.floor((duration - (horas * 3600)) / 60),
+        segundos = duration - (horas * 3600) - (minutos * 60),
+        tiempo = "";
+
+    if (horas < 10) { horas = "0" + horas; }
+    if (minutos < 10) { minutos = "0" + minutos; }
+    if (segundos < 10) { segundos = "0" + segundos; }
+
+    if (horas === "00") {
+        tiempo = minutos + "<span style='color: #e30052'>:</span>" + segundos;
+    } else {
+        tiempo = horas + "<span style='color: #e30052'>:</span>" + minutos + "<span style='color: #e30052'>:</span>" + segundos;
+    }
+    return tiempo;
+}
+
+// Convertir el tiempo del formato "00:00:00" a segundos
+function getSecondsTime(duration) {
+    if(typeof duration === "number") return duration;
+    if(typeof duration !== "string") return 0;
+
+    duration = duration.split(":");
+    let time = 0;
+
+    if(duration.length === 2) {
+        time += Number(duration[0]) * 60;
+        time += Number(duration[1]);
+    } else if(duration.length === 3) {
+        time += Number(duration[0]) * 3600;
+        time += Number(duration[1]) * 60;
+        time += Number(duration[2]);
+    }
+
+    return time;
+}
+
+// carga el recurso de la pista, ya sea la direción del mp3 o el base64
+function setSource(source, type) {
+    let isNotStream = RadioCassette.reproductorTipo !== ReproductorTipo.Stream,
+        isType = type;
+        
+    return new Promise((resolve, reject) => {
+        if(isNotStream){
+            // Si la dirección contiene extensión js|txt|json o si es una url normal sin extensión
+            // Se carga el valor del base64
+            if(source.match(/\w+.(txt|json|js)/g) || (!source.match(/.+(mp3|ogg|opus|aac|m4a)/g)) && !isType){
+                let script = document.createElement("script");
+
+                script.onload = _ => {
+                    player.src = base64;
+                    script.remove();
+                    resolve();
+                }
+
+                script.src = source;
+                document.body.appendChild(script);
+            }else if(source.match(/.+(mp3|ogg|opus|aac|m4a)/g) || isType){
+                // cargar el archivo mp3|ogg|opus|aac|m4a
+                player.src = source;
+                resolve();
+            }else{
+                // capturar un error
+                reject();
+            }
+        }else{
+            player.src = source;
+            resolve();
+        }
+    });
+}
+
+function updateIndicators(currentTime, duration){
+    let comodin = (duration >= 3600) ? "00<span style='color: #e30052'>:</span>" : "";
+
+    currentTimeIndicator.innerHTML = comodin + getReadableTime(currentTime);
+
+    // Actualizar el porcentaje de avance
+    progressValue.dispatchEvent(new CustomEvent('updateprogresssong', {
+        detail: {
+            value: Math.floor((currentTime * 100) / (duration - 1))
+        }
+    }));
+
+    // Para actualizar el tiempo restante si está habilitado
+    if(RadioCassette.tiempoFinal == TiempoFinal.TiempoRestante){
+        let durationTotal = (RadioCassette.reproductorTipo === ReproductorTipo.Playlist) ? player.duration : getSecondsTime(RadioCassette.valorTiempoGeneral);
+        
+        lastTimeIndicator.dispatchEvent(
+            new CustomEvent('updateTiempoRestante', {
+                detail: {
+                    duration: durationTotal - currentTime,
+                    total: durationTotal
+                },
+                bubbles: true,
+                cancelable: true
+            }));
+    }
+}
+
+// Actualizar las etiquetas y tiempos a partir del tiempo actual de la pista
+function onCurrentTimeInterval() {
+    let duration = (RadioCassette.mostrarTiempoGeneral == 1) ? getSecondsTime(RadioCassette.valorTiempoGeneral) : player.duration;
+
+    if(RadioCassette.reproductorTipo !== ReproductorTipo.Stream) {
+        currentTime = (RadioCassette.mostrarTiempoGeneral == 1) ? acumulateTime + player.currentTime : player.currentTime;
+    }else{
+        currentTime++;
+    }
+
+    if(RadioCassette.reproductorTipo === ReproductorTipo.Stream){
+        if(currentTime >= duration){
+            updateIndicators(duration, duration);
+            
+            setTimeout(() => onClickBtnStop(), 500);
+
+            setTimeout(() => { setSource(RadioCassette.url).then(() => player.play()) }, 1500);
+        }
+    }
+   
+    //currentTime++;
+    //console.log(currentTime, player.currentTime);
+    if(currentTime < duration) {
+        //currentTime++;
+        updateIndicators(currentTime, duration);
+    }
+}
+
+// Actualiza la etiqueta del tiempo restante
+function onUpdateTiempoRestante(e) {
+    let comodin = (e.detail.total >= 3600 && e.detail.duration < 3600) ? "00<span style='color: #e30052'>:</span>" : "";
+    lastTimeIndicator.innerHTML = comodin + getReadableTime(e.detail.duration);
+}
+
+// Al cargar la pista actual, carga la duración y calcula el ángulo de rotación del brazo
+function onLoadPlayerData() {
+    let duration = (RadioCassette.reproductorTipo == ReproductorTipo.Playlist) ? player.duration : getSecondsTime(RadioCassette.valorTiempoGeneral);
+
+    duration = (RadioCassette.mostrarTiempoGeneral == 1) ? getSecondsTime(RadioCassette.valorTiempoGeneral) : duration;
+    
+    let comodin = (duration >= 3600) ? "00<span style='color: #e30052'>:</span>" : "";
+
+    currentTimeIndicator.innerHTML = comodin + getReadableTime(0);
+    lastTimeIndicator.innerHTML = getReadableTime(duration);
+    isLoaded = true;
+    
+    if(isStarted && RadioCassette.mostrarTiempoGeneral != 1) {
+        setTimeout(function(){ 
+            //arm.style.transform = 'rotate('+agujaRotacion(Tocadiscos.aguja).inicio+'deg)'; 
+        }, 1500);
+    }
+}
+
+// Al iniciar la reproducción de la pista
+function onPlayPlayer() {
+    player.isStop = false;
+    let shuffleCheck = [EstiloReproduccion.Shuffle, EstiloReproduccion.Sattolo];
+
+    if(!isStarted){
+        // Verificar si se va a reproducir por lista de canciones
+        if(RadioCassette.reproductorTipo === ReproductorTipo.Playlist){
+            if(RadioCassette.canciones.length > 0){
+                if(shuffleCheck.indexOf(RadioCassette.estiloReproduccion) !== -1){
+                    if(RadioCassette.estiloReproduccion === EstiloReproduccion.Shuffle){
+                        RadioCassette.canciones.shuffle();
+                    }else{
+                        RadioCassette.canciones.sattolo();
+                    }
+                    
+
+                    console.log("----- Lista Inicial -----");
+                    console.log(RadioCassette.canciones);
+                }
+
+                // Valores de la canción actual en reproducción
+                currentSong = {
+                    index: songIndex,
+                    path: RadioCassette.canciones[songIndex].path,
+                    type: (typeof RadioCassette.canciones[songIndex].type == "undefined") ? false : true
+                };
+            }
+
+         // En caso de que se este reproduciendo por medio del enlace de la radio
+        }else if(RadioCassette.reproductorTipo === ReproductorTipo.Stream){
+            currentSong = {
+                index: -1,
+                path: RadioCassette.url
+            }
+        }
+
+        setSource(currentSong.path, currentSong.type)
+            .then(() => player.play())
+            .catch(() => console.error("Error on load source: ", currentSong.path));
+
+        isStarted = true;
+    }
+
+    btnPlay.classList.add("active");
+}
+
+// Pausar la reproducción
+function onPausePlayer() {
+    //clearInterval(currentTimeInterval);
+}
+
+// Al finalizar la pista actual
+function onEndedPlayerData() {
+    let timeOutValue = 2000,
+        oldTime = currentTime;
+
+    Clean();
+
+    if(RadioCassette.mostrarTiempoGeneral == 1 && !player.isStop){
+        timeOutValue = 0;
+        currentTime = oldTime;
+        acumulateTime += player.duration;
+        currentTimeIndicator.innerHTML = getReadableTime(currentTime);
+        lastTimeIndicator.innerHTML = getReadableTime(RadioCassette.valorTiempoGeneral);
+    }
+    
+    let playlistLength = RadioCassette.canciones.length;
+
+    // Al reproducir por lista de canciones
+    if(RadioCassette.reproductorTipo == ReproductorTipo.Playlist) {
+        if(songIndex == playlistLength && EstiloReproduccion.Linear){
+            onClickBtnStop();
+            return;
+        }
+
+        // Si esta en modo LinearLoop o Shuffle, reinicia la reproducción de la lista al inicio
+        if(RadioCassette.estiloReproduccion != EstiloReproduccion.Linear){
+            let shuffleCheck = [EstiloReproduccion.Shuffle, EstiloReproduccion.Sattolo];
+            songIndex = (songIndex + 1 == playlistLength) ? -1 : songIndex;
+
+            if(songIndex == -1 && shuffleCheck.indexOf(RadioCassette.estiloReproduccion) !== -1){
+                oldShuffleSongs = RadioCassette.canciones.copy();
+
+                let lastSOngFromOldList = oldShuffleSongs[oldShuffleSongs.length - 1];
+
+                do{
+                    if(RadioCassette.estiloReproduccion === EstiloReproduccion.Shuffle){
+                        RadioCassette.canciones.shuffle();
+                    }else{
+                        RadioCassette.canciones = oldShuffleSongs.copy();
+                        RadioCassette.canciones.sattolo();
+                    }
+                }while(RadioCassette.canciones[0] === lastSOngFromOldList);
+
+                console.log("----- Lista Antigua -----");
+                console.log(oldShuffleSongs);
+                console.log("----- Lista Nueva -----");
+                console.log(RadioCassette.canciones);
+            }
+        }
+
+        // Se obtiene los valores de la siguiente canción
+        if(songIndex < playlistLength - 1){
+            songIndex++;
+            currentSong = {
+                index: songIndex,
+                path: RadioCassette.canciones[songIndex].path,
+                type: (typeof RadioCassette.canciones[songIndex].type == "undefined") ? false : true
+            }
+
+            setTimeout(() => {
+                setSource(currentSong.path, currentSong.type)
+                    .then(() => player.play())
+                    .catch(() => console.error("Error on load source: ", currentSong.path));
+            }, 2000);
+        }
+    }
+}
+
+// Obtener el total de duración para el modo de mostrarTiempoTotal
+function getTotalDurationForPlaylist() {
+    if(RadioCassette.mostrarTiempoGeneral == 1 && RadioCassette.reproductorTipo == ReproductorTipo.Playlist){
+        let durationSum = 0;
+
+        for(let i=0; i < RadioCassette.canciones.length; i++){
+            durationSum += getSecondsTime(RadioCassette.canciones[i].duration);
+            RadioCassette.valorTiempoGeneral = durationSum;
+        }
+
+    }
+
+    btnPlay.removeAttribute("disabled");
+    btnPause.removeAttribute("disabled");
+    btnBack.removeAttribute("disabled");
+    btnForward.removeAttribute("disabled");
+    btnStop.removeAttribute("disabled");
+}
+
+// Presionar el botón de inicio
+function onClickBtnStart(e) {
+    if(RadioCassette.reproductorTipo === ReproductorTipo.Stream){
+        currentTimeInterval = setInterval(onCurrentTimeInterval, 1000);
+        player.muted = false;
+    }
+
+    player.play();
+    btnPlay.classList.add("active");
+}
+
+// Presionar el botón de pausa
+function onCLickBtnPause() {
+    if(RadioCassette.reproductorTipo === ReproductorTipo.Stream){
+        clearInterval(currentTimeInterval);
+        player.muted = true;
+    }
+
+    if(btnPlay.classList.contains("active") && RadioCassette.reproductorTipo !== ReproductorTipo.Stream){
+        player.pause();
+    }
+
+    btnPlay.classList.remove("active");
+}
+
+// Presionar el botón de retroceder
+function onClickBtnBack() {
+    if(RadioCassette.reproductorTipo !== ReproductorTipo.Stream){
+        songIndex = (songIndex - 1 <= 0) ? 0 : (songIndex - 1);
+
+        player.pause();
+
+        if(songIndex === 0){
+            onClickBtnStop();
+            setTimeout(onClickBtnStart, 100);
+        }else{
+            oldTime = currentTime - player.currentTime;
+
+            Clean();
+
+            if(RadioCassette.mostrarTiempoGeneral == 1){
+                currentTime = oldTime;
+                acumulateTime -= player.duration;
+                currentTimeIndicator.innerHTML = getReadableTime(currentTime);
+                lastTimeIndicator.innerHTML = getReadableTime(RadioCassette.valorTiempoGeneral);
+            }
+
+            // Valores de la canción actual en reproducción
+            currentSong = {
+                index: songIndex,
+                path: RadioCassette.canciones[songIndex].path,
+                type: (typeof RadioCassette.canciones[songIndex].type == "undefined") ? false : true
+            };
+
+            setSource(currentSong.path, currentSong.type)
+                .then(() => player.play())
+                .catch(() => console.error("Error on load source: ", currentSong.path));
+        }
+    }
+}
+
+// Presionar el botón de avanzar
+function onClickBtnForward() {
+    if(RadioCassette.reproductorTipo !== ReproductorTipo.Stream){
+        player.currentTime = player.duration;
+    }
+}
+
+// Actualiza el valor de la etiqueta del porcentaje de progreso
+function onUpdateProgressSong(e){
+    let value = e.detail.value,
+        _width = Number($('.bars').css('width').split('px').join(''));
+
+    value = (value <= 100) ? value : 100;
+    _width = ((_width - Number($('.progress-song').css('width').split('px').join(''))) * value) / 100;
+
+    progressValue.textContent = value;
+    progressBar.css({ width:  _width+'px'});
+}
+
+// Presionar el botón de detener
+function onClickBtnStop(e) {
+    player.stop();
+    player = new _Audio();
+
+    if(RadioCassette.reproductorTipo === RadioCassette.Stream)
+        clearInterval(currentTimeInterval);
+}
+
+// Reinicar los valores de las distintas etiquetas
+function Clean() {
+    player.pause();
+    player.duration = 0;
+
+    currentTime = 0;
+
+    currentTimeIndicator.innerHTML = getReadableTime(0);
+    lastTimeIndicator.innerHTML = getReadableTime(0);
+    progressValue.textContent = 0;
+    progressBar.css({ width:  0});
+
+    btnPlay.classList.remove("active");
+}
+
+// Audio Prototypes
+function _Audio() {
+    let _audio = new Audio();
+
+    // - Load array of song's
+    _audio.songIndex = -1;
+    // Detener la reproducción
+    _audio.isStop = true;
+    _audio.stop =function() {
+        this.isStop = true;
+        isStarted = false;
+        songIndex = 0;
+        acumulateTime = 0;
+        currentTime = 0;
+        Clean();
+
+        setTimeout(() => {
+            currentTimeIndicator.innerHTML = getReadableTime(0);
+            progressValue.textContent = "0";
+        }, 10);
+    }
+
+    _audio
+        .on("loadeddata", onLoadPlayerData)
+        .on("timeupdate", (RadioCassette.reproductorTipo !== ReproductorTipo.Stream) ? onCurrentTimeInterval : function(){})
+        .on("play", onPlayPlayer)
+        .on("pause", onPausePlayer)
+        .on("ended", onEndedPlayerData);
+
+    return _audio;
+}
+
+
 window.on('load', () => {
+    // Cargar las configuraciones del tocadiscos
+    titleText.textContent = RadioCassette.textoTitulo;
+    titleImage.src = RadioCassette.imagenTitulo;
+
+    sloganText.textContent = RadioCassette.textoEslogan;
+    sloganImage.src = RadioCassette.imagenEslogan;
+
+    RadioCassette.mostrarTiempoGeneral = (RadioCassette.reproductorTipo == ReproductorTipo.Stream) ? 1 : RadioCassette.mostrarTiempoGeneral;
+
+    player = new _Audio();
+
+    getTotalDurationForPlaylist();
+
+    btnPlay.on('click', onClickBtnStart);
+    btnPause.on('click', onCLickBtnPause);
+    btnBack.on('click', onClickBtnBack);
+    btnForward.on('click', onClickBtnForward);
+    btnStop.on("click", onClickBtnStop);
+
+    progressValue.on('updateprogresssong', onUpdateProgressSong);
+    lastTimeIndicator.on('updateTiempoRestante', onUpdateTiempoRestante);
 });
